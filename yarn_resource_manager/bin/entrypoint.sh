19dacb192d5b358c219a91d5096e1f04d923627d
Elias Levy <fearsome.lucidity@gmail.com>
2015-10-30 13:20:32 -0700

    Log to the console in addition to the log.

--- yarn_resource_manager/bin/entrypoint.sh
+++ yarn_resource_manager/bin/entrypoint.sh
@@ -8,6 +8,6 @@ sed -e "s|\${ZOOKEEPER_ADDRESS}|${ZOOKEEPER_ADDRESS:-zookeeper:2181}|g" \
 export YARN_CONF_DIR=/hadoop/etc/hadoop
 export YARN_LOG_DIR=/hadoop/logs
 export YARN_LOGFILE=yarn-resourcemanager.log
-export YARN_ROOT_LOGGER=${YARN_ROOT_LOGGER:-INFO,RFA}
+export YARN_ROOT_LOGGER=${YARN_ROOT_LOGGER:-INFO,RFA,console}
 
 exec "$@"
