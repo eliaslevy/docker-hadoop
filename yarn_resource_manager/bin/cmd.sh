36c597a636f727d61785bff55d5139918d620f29
Elias Levy <fearsome.lucidity@gmail.com>
2015-10-30 12:28:32 -0700

    Execute the resource manager in the foreground.

--- yarn_resource_manager/bin/cmd.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-
-yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager
-
-tail -Fn +0 /hadoop/logs/yarn-*-resourcemanager-*.out &
-
-wait `cat /tmp/yarn-*-resourcemanager.pid`
-
-sleep 5
--- yarn_resource_manager/bin/entrypoint.sh
+++ yarn_resource_manager/bin/entrypoint.sh
@@ -5,4 +5,9 @@ sed -e "s|\${ZOOKEEPER_ADDRESS}|${ZOOKEEPER_ADDRESS:-zookeeper:2181}|g" \
     -e "s|\${ZOOKEEPER_ROOT}|${ZOOKEEPER_ROOT:-/yarn_resource_manager}|g" \
     -i /hadoop/etc/hadoop/yarn-site.xml
 
+export YARN_CONF_DIR=/hadoop/etc/hadoop
+export YARN_LOG_DIR=/hadoop/logs
+export YARN_LOGFILE=yarn-resourcemanager.log
+export YARN_ROOT_LOGGER=${YARN_ROOT_LOGGER:-INFO,RFA}
+
 exec "$@"
